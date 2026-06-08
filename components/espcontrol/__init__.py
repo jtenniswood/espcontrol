da507f1f4173290440934ba477975ced5699c714
jtenniswood <james@tenniswood.co.uk>
2026-06-08 12:15:44 +0100

    Include espcontrol helpers in generated firmware

--- components/espcontrol/__init__.py
+++ components/espcontrol/__init__.py
@@ -24,6 +24,7 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     comp_dir = os.path.dirname(os.path.abspath(__file__))
     cg.add_build_flag(f"-I{comp_dir}")
+    cg.add_global(cg.RawStatement('#include "button_grid.h"'), prepend=True)
     if config[CONF_ACTION_RESPONSES]:
         cg.add_define("USE_API_HOMEASSISTANT_ACTION_RESPONSES")
         cg.add_define("USE_API_HOMEASSISTANT_ACTION_RESPONSES_JSON")
